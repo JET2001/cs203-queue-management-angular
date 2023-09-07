b32c5e73b33030b0db3b701fed5b6310b254cf93
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-07 13:34:42 +0800

    Added sample injection of event service

--- src/app/feature/events-register/pages/group-registration/group-registration.component.ts
+++ src/app/feature/events-register/pages/group-registration/group-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { StoreEventInfoService } from 'src/app/shared/services/store-event-info.service';
 
 @Component({
   selector: 'app-group-registration',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./group-registration.component.scss']
 })
 export class GroupRegistrationComponent {
+  eventID!: number | undefined;
+  userID!: number | undefined;
+  groupID!: number | undefined;
+  constructor(
+    private storeEventInfoService: StoreEventInfoService,
+    private router: Router
+  ){}
 
+  ngOnInit(): void {
+   this.eventID = this.storeEventInfoService.eventInfo.eventID;
+   this.userID = this.storeEventInfoService.eventInfo.userID;
+   this.groupID = this.storeEventInfoService.eventInfo.groupID;
+  }
+
+  confirm(): void {
+    // Save to DB
+
+    // Route back to login
+    this.router.navigate(['/events']);
+  }
 }
--- src/app/feature/events/pages/view-event-info/view-event-info.component.ts
+++ src/app/feature/events/pages/view-event-info/view-event-info.component.ts
@@ -24,4 +24,5 @@ export class ViewEventInfoComponent implements OnInit {
     }
   }
 
+
 }
