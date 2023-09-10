ad4eb7ef1a36adf87cde4de168d3acf5922ba470
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-10 11:05:49 +0800

    Added vacant slot messages

--- src/app/feature/events/components/group-member-list/group-member-list.component.ts
+++ src/app/feature/events/components/group-member-list/group-member-list.component.ts
@@ -9,7 +9,20 @@ export class GroupMemberListComponent {
   @Input() memberEmailList : string[] = [];
   @Input() confirmList: number[] = [];
 
+  ngOnInit(): void{
+    if (this.memberEmailList.length != this.confirmList.length) {
+      console.log("GroupMemberListError: MemberEmailList is not the same as confirmlist!");
+    }
+  }
   hasConfirmed(index: number): boolean{
     return this.confirmList[index] == 1;
+  }
+
+  getNumVacancies(): number {
+    return 3 - this.memberEmailList.length;
+  }
+
+  hasVacancies(): boolean {
+    return this.memberEmailList.length < 3;
   }
 }
