2e463e6fef3974ffad817e6d3674cf27ee5f4890
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-13 05:12:15 +0800

    Added routing in the upcoming concerts component

--- src/app/feature/home/components/upcoming-concerts/upcoming-concerts.component.ts
+++ src/app/feature/home/components/upcoming-concerts/upcoming-concerts.component.ts
@@ -1,24 +1,34 @@
 import { Event } from 'src/app/models/event';
 import { GetEventInfoService } from '../../../../shared/services/get-event-info/get-event-info-service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StoreEventInfoService } from 'src/app/shared/services/store-event-info/store-event-info.service';
 
 @Component({
   selector: 'app-upcoming-concerts',
   templateUrl: './upcoming-concerts.component.html',
   styleUrls: ['./upcoming-concerts.component.scss'],
 })
-export class UpcomingConcertsComponent implements OnInit{
+export class UpcomingConcertsComponent implements OnInit {
+  @Input() userID : number | undefined = undefined;
   events: Event[];
-  constructor(private getEventInfoService: GetEventInfoService, private router: Router) {}
+  constructor(
+    private getEventInfoService: GetEventInfoService,
+    private storeEventInfoService: StoreEventInfoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-      this.getEventInfoService.loadAllCarousellEvents().then((events) => {
-        this.events = events;
-      })
+    this.getEventInfoService.loadAllCarousellEvents().then((events) => {
+      this.events = events;
+    });
   }
 
   handleButtonClick(eventID: number): void {
+    this.storeEventInfoService.eventInfo = {
+      eventID: eventID,
+      userID: this.userID
+    };
     this.router.navigate(['/events']);
   }
 }
