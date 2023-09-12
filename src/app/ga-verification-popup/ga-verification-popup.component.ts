bfc635d69bd84eb2b80902ad407ca015dcdea08d
Clarissa Toh <clarissatoh03@gmail.com>
2023-09-12 19:33:20 +0800

    add form control for the input field in ga popup

--- src/app/ga-verification-popup/ga-verification-popup.component.ts
+++ src/app/ga-verification-popup/ga-verification-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -7,5 +8,16 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./ga-verification-popup.component.scss'],
 })
 export class GaVerificationPopupComponent {
+  authenticationFC: FormControl = new FormControl('', []);
+
   constructor(public activeModal: NgbActiveModal) {}
+
+  handleAuthenticateClick(): void {
+    if (this.authenticationFC.invalid) {
+      // console.log('error');
+    }
+    else {
+      // console.log(this.authenticationFC.value);
+    }
+  }
 }
--- src/app/shared/components/input-field/input-field.component.ts
+++ src/app/shared/components/input-field/input-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, forwardRef } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
 import { FormControl, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
 
 @Component({
@@ -13,13 +13,13 @@ import { FormControl, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
     },
   ],
 })
-export class InputFieldComponent {
+export class InputFieldComponent implements OnInit{
   @Input() inputType: 'mobile' | 'text' | 'email' | 'password' | 'auth-code';
   @Input() placeholder: string = '';
   @Input() isRequired: boolean = true;
-  inputValue: FormControl = new FormControl('', []);
+  @Input() inputValue: FormControl = new FormControl('', []);
 
-  constructor() {
+  ngOnInit(): void {
     this.inputValue.valueChanges.subscribe(() => {
       this.validateInput();
     });
