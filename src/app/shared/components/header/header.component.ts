6b7f7e7837072556d35016b70945c6dc56cb8314
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-16 09:06:56 +0800

    Added a concerts button that routes to the landing page

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,16 +2,20 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { StoreEventInfoService } from '../../services/store-event-info/store-event-info.service';
 import { Users } from 'src/app/mock-db/MockDB';
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   @Output() verifiedUserLoggingIn = new EventEmitter<void>();
   emailID: string | undefined = undefined;
-  constructor(private authService: AuthenticationService) {}
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.emailID = this.authService.email;
@@ -25,15 +29,19 @@ export class HeaderComponent implements OnInit{
     let userInputStr: string | null = prompt(
       'Choose 1 user ID to log in as 0 for Jon, 1 for David, 2 for Clarissa, 3 for Ben, 4 for Ryan, and -1 to return: '
     );
-    if(!userInputStr) return;
+    if (!userInputStr) return;
     let userInput: number | undefined = Number.parseInt(userInputStr);
     if (userInput == undefined || userInput <= -1 || userInput >= 5) return;
 
     this.authService.userID = userInput;
     this.emailID = this.authService.email;
 
-    if(this.authService.isVerified){
+    if (this.authService.isVerified) {
       this.verifiedUserLoggingIn.emit();
     }
+  }
+
+  handleConcertsButtonClick(): void {
+    this.router.navigate(['/home']);
   }
 }
