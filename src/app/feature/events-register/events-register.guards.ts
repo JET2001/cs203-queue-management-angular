de66e6b229d672c1150f5b44acb23b385d4910f8
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-22 17:16:46 +0800

    Added guards for events-register module and group register

---
+++ src/app/feature/events-register/events-register.guards.ts
@@ -0,0 +1,72 @@
+import { Injectable, inject } from '@angular/core';
+import { CanActivateChildFn, CanActivateFn, Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { GetRegistrationGroupService } from 'src/app/shared/services/get-registration-group/get-registration-group.service';
+import { StoreEventInfoService } from 'src/app/shared/services/store-event-info/store-event-info.service';
+import { RegGroup } from 'src/app/models/reg-group';
+import { StoreRegistrationGroupInfoService } from 'src/app/shared/services/store-registration-group-info/store-registration-group-info.service';
+import { Location } from '@angular/common';
+
+/**
+ * Guard for events register module
+ * Used by the canActivateChildFn in app-routing.ts
+ * @returns CanActivateChildFn (boolean)
+ */
+export function eventRegisterGuard(): CanActivateChildFn {
+  return () => {
+    const authService : AuthenticationService = inject(AuthenticationService);
+    const storeEventInfoService: StoreEventInfoService = inject(StoreEventInfoService);
+
+    // User is not verified --> reject
+    if (!authService.isVerified) return denyAccess();
+
+    // EventID is not properly loaded --> reject
+    if (storeEventInfoService.eventInfo.eventID == undefined) return denyAccess();
+
+    return grantAccess();
+  }
+}
+
+/**
+ * Guard for Group Registration page
+ * @returns CanActivateFn (boolean)
+ */
+export function groupRegisterGuard(): CanActivateFn {
+  return async() => {
+    const authService: AuthenticationService = inject(AuthenticationService);
+    const getRegGroupInfoService: GetRegistrationGroupService  = inject(GetRegistrationGroupService);
+    const storeEventInfoService: StoreEventInfoService = inject(StoreEventInfoService);
+    const storeRegGroupService: StoreRegistrationGroupInfoService = inject(StoreRegistrationGroupInfoService);
+
+    const eventID = storeEventInfoService.eventInfo.eventID;
+    const userID = authService.userID;
+
+    let userRegGroup : RegGroup | undefined;
+    await getRegGroupInfoService.getRegGroupOfUser(eventID!, userID).then(
+      (regGroup : RegGroup | undefined) => userRegGroup = regGroup
+    );
+    // If user tries to modify group but does not have an actual group, reject
+    if (storeRegGroupService.modifyGroup && userRegGroup == undefined) return denyAccess();
+    // If user is not trying to modify group but already has a registered group, reject
+    if (!storeRegGroupService.modifyGroup && userRegGroup != undefined) return denyAccess();
+
+    return grantAccess();
+  }
+}
+
+/**
+ * Guard against the queue register page
+ */
+// export function queueRegisterGuard(): CanActivateFn {
+
+// }
+
+function denyAccess(): boolean{
+  const location: Location = inject(Location);
+  location.back(); // back to previous page
+  return false;
+}
+
+function grantAccess(): boolean {
+  return true;
+}
--- src/app/feature/events-register/events-register.routing.ts
+++ src/app/feature/events-register/events-register.routing.ts
@@ -2,11 +2,13 @@ import { Routes } from "@angular/router";
 import { GroupRegistrationComponent } from "./pages/group-registration/group-registration.component";
 import { RegistrationPreviewComponent } from "./pages/registration-preview/registration-preview.component";
 import { QueueTimingsComponent } from "./pages/queue-timings/queue-timings.component";
+import { groupRegisterGuard } from "./events-register.guards";
 
 export const eventRegisterRoutes : Routes = [
   {
     path: 'group',
-    component: GroupRegistrationComponent
+    component: GroupRegistrationComponent,
+    canActivate: [groupRegisterGuard()]
   },
   {
     path: 'queue',
