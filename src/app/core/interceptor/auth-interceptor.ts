b51a4c349c28c15f3d870cb18f9b131fd85f2532
Jonathan Edward Teo <91585955+JET2001@users.noreply.github.com>
2023-09-24 21:14:49 +0800

    Feature/cs203 161 196 234/login api local storage login service dev (#38)
    * Fixed models, database and discrepancies between integer IDs and string IDs
    
    * WIP - tried using promises but it didn't work - i get the whole observable object -.- , observables are loading, but i think at the time that ngOnInit is executed the arrays have not been filled.
    
    * OMG IT WORKS! My panic attack can leave me now
    
    Okay so i found that when I subscribe to an observable in a service is different when i subscribe to the observable in the component.
    
    Initially i thought subscribing to the service in the component would reduce boilerplate, but then i think that means that the component is not listening for changes.
    
    * Converted all base-api services to return an observable
    
    * Made image path consistent
    
    * Added bearer word for authorization
    
    * Updated the token interceptor with excluded paths
    
    * Created a local storage manager
    
    * Base api service assumes that the responsetype must be json, so overwriting it is necessary in order for us to process the JWT
    
    * Why can't i update the userID in the header ah?
    
    * fixed email not showing in header
    
    ---------
    
    Co-authored-by: Clarissa Toh <clarissatoh03@gmail.com>

--- src/app/core/interceptor/auth-interceptor.ts
+++ src/app/core/interceptor/auth-interceptor.ts
@@ -7,24 +7,42 @@ import {
 import { Injectable } from '@angular/core';
 import { AuthenticationService } from '../services/authentication/authentication.service';
 import { Observable } from 'rxjs';
+import { baseURL } from '../constants/api-paths';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthenticationService) {}
 
+  // Intercept for most urls, but we need to exclude some urls, for instance landing page. Here is where I exclude the injection of some urls
+  excludePaths: Array<string> = [
+    "users/auth/login",
+    "events/.+"
+  ];
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    // Check if this is an API call, if it is not, do not intercept
+    if (!(req.url.includes(`${baseURL}`))){
+      return next.handle(req);
+    }
+    // Check if this is inside the paths to exclude, if it is, do not intercept.
+    for (let pathToExclude of this.excludePaths){
+      let pathRegEx = new RegExp(pathToExclude);
+      if (pathRegEx.test(req.url)){
+        return next.handle(req);
+      }
+    }
     // Get the authtoken from the service
-    const authToken: string | undefined = this.authService.authToken;
+    const authToken: string | null = this.authService.retrieveAuthToken;
 
     // Clone the request and replace the original headers with
     // cloned headers, updated with the authorization
     const authReq =
-      authToken != undefined
+      (authToken != null)
         ? req.clone({
-            setHeaders: { Authorization: authToken },
+            setHeaders: { Authorization: "Bearer " + authToken },
           })
         : req;
 
