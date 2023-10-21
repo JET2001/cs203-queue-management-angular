1840c56b9d58eb6273ba76178dc8fe134badc12b
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-10-22 02:13:09 +0800

    Updated base-component to call the actual spinner.show() method if the spinner is not already spinning

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -7,16 +7,25 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class BaseComponent {
-  
+
+  private _isSpinning: boolean = false;
   constructor(protected spinner: NgxSpinnerService){}
 
   public spinnerShow() {
-    this.spinner.show();
+    if(!this._isSpinning){
+      this.spinner.show();
+      this._isSpinning = true;
+    }
   }
 
   public spinnerHide() {
     setTimeout(() => { // spinner stops 1 second after function has been called.
       this.spinner.hide();
+      this._isSpinning = false;
     }, 1000);
+  }
+
+  protected get isSpinning(): boolean {
+    return this._isSpinning;
   }
 }
