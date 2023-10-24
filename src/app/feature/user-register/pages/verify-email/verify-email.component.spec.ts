4d52c6d775e5d8cb6f2d1e00c92e6471cfff6dce
Clarissa Toh <clarissatoh03@gmail.com>
2023-10-24 13:56:21 +0800

    resolve merge conflict

--- src/app/feature/user-register/pages/verify-email/verify-email.component.spec.ts
+++ src/app/feature/user-register/pages/verify-email/verify-email.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
 import { VerifyEmailComponent } from './verify-email.component';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
