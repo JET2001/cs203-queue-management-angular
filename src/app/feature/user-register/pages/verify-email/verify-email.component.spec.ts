66337a4354c2235af6d3d825405e66c86403926b
Clarissa Toh <clarissatoh03@gmail.com>
2023-10-24 14:05:00 +0800

    remove duplicate import

--- src/app/feature/user-register/pages/verify-email/verify-email.component.spec.ts
+++ src/app/feature/user-register/pages/verify-email/verify-email.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RouterTestingModule } from '@angular/router/testing';
 import { VerifyEmailComponent } from './verify-email.component';
-import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 import { MessagesModule } from 'primeng/messages';
